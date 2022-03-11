3b2c32a27274d67481f264c4d565561897da2699
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-11 10:44:07 -0800

    created a route for each individual video/id

--- routes/videos.js
+++ routes/videos.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const fs = require("fs")
 
 
-const selectVideo = () =>{
+const selectVideos = () =>{
     const readVideos =  JSON.parse(fs.readFileSync("./data/videos.json"))
     return readVideos
 }
 //route for videos
 router.get("/", (req, res) => {
-    res.send(selectVideo())
+    res.send(selectVideos())
+    .status(200)
+})
+
+//route for the individual video id
+router.get("/:id", (req, res) => {
+    const selectedVideo = selectVideos().find((video) => video.id == req.params.id)
+    console.log(selectedVideo)
+    res.send(selectedVideo)
     .status(200)
 })
 
