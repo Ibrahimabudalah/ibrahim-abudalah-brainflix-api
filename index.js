f9df4c1fb16570afe4f3ef603fcd2a6f8d176d8c
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-03-11 13:28:08 -0800

    created the api info .env and installed cors to link the server to the front end

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config()
+const { PORT, BACKEND_URL } = process.env
+const cors = require("cors")
+
 
 
 //middleware
@@ -7,12 +11,15 @@ app.use((req, res, next) => {
     console.log("Incoming request")
     next()
 })
+app.use(cors())
+
 
 //routes
 const route = require("./routes/videos")
 app.use("/videos", route)
 
 //listener
-app.listen(8080, () =>{
+app.listen(process.env.PORT, () =>{
+    console.log(process.env.BACKEND_URL+`:${PORT}`)
     console.log("server is running on port 8080")
 })
